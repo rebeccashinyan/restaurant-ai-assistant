f6ab7e74d51f084a5c4f036445f40371883142ba
rebeccarebecc <s10421@kcis.com.tw>
2026-05-20 12:40:59 +0800

    add navbar for all pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,6 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-orange-50 p-10">
-      <nav className="flex gap-6 mb-10 text-lg">
-        <Link href="/">Home</Link>
-        <Link href="/menu">Menu</Link>
-        <Link href="/location">Location</Link>
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/ask">Ask Sakura</Link>
-      </nav>
 
       <h1 className="text-5xl font-bold text-orange-600">
         Sakura Bloom Matcha
